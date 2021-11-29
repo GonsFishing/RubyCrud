26ac2b502103ef5bcd86cd7d19000310a1bf7c38
Sixten Gons Larsson <sixtenglarsson@gmail.com>
2021-11-29 12:57:12 +0100

    added multiple dependencies

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
 	has_many :songs
+	has_many :albums
 	has_many :image_elements
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
 	belongs_to :artist
+	belongs_to :album
 	belongs_to :user
 	has_many :image_elements
-	
 end
